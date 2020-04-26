171f4dafb829fefab76432254316868a21883531
yyj <834929499@qq.com>
2020-04-26 22:54:18 +0800

    fix: 解决端口冲突问题

--- request-node.js
+++ request-node.js
@@ -3,16 +3,6 @@ const http = require('http');
 const fs = require('fs');
 var child_process = require('child_process');
 
-const method = "GET";
-
-const url = "https://park.fuwu178.cn/api/rest/user/me";
-
-const headers = {
-    "Content-Type": 'application/x-www-form-urlencoded; charset=UTF-8',
-};
-
-const body = null;
-
 function assignToOptions(method, url, headers) {
     /(https?:)\/\/([^/:]+):?(\d+)?(.*)/.test(url);
     var options = { headers, method };
@@ -20,6 +10,7 @@ function assignToOptions(method, url, headers) {
     options.hostname = RegExp.$2;
     options.port = RegExp.$3 || 443;
     options.path = RegExp.$4;
+    options.rejectUnauthorized = false;
     console.log(options);
     return options;
 }
@@ -60,36 +51,57 @@ function ajax(method, url, headers, body, sendResponse) {
     req.end();
 }
 
+function startServer(_port) {
+    var port = _port || parseInt(process.argv[2] || 9394);
 
-var server = http.createServer(function (req, res) {
-    switch (req.url) {
-        case "/index.html":
-            var homepage = fs.readFileSync(__dirname + '/index.html').toString();
-            res.end(homepage);
-            break;
-        case "/api":
-            var reqBuffer = "";
-            req.on("data", (chunk) => {
-                reqBuffer += chunk;
-            })
-            req.on("end", () => {
-                var reqData = JSON.parse(reqBuffer);
-                ajax(reqData.method, reqData.url, JSON.parse(reqData.headers), reqData.body,
-                    (data) => {
-                        res.end(data);
-                    });
-            })
-            break;
-    }
-});
+    var server = http.createServer(function (req, res) {
+        switch (req.url) {
+            case "/index.html":
+                var homepage = fs.readFileSync(__dirname + '/index.html').toString();
+                res.end(homepage);
+                break;
+            case "/api":
+                var reqBuffer = "";
+                req.on("data", (chunk) => {
+                    reqBuffer += chunk;
+                })
+                req.on("end", () => {
+                    var reqData = JSON.parse(reqBuffer);
+                    ajax(reqData.method, reqData.url, JSON.parse(reqData.headers), reqData.body,
+                        (data) => {
+                            res.end(data);
+                        });
+                })
+                break;
+        }
+    });
 
-server.listen(9394, () => {
-    console.log('服务启动(。・∀・)ノ http://127.0.0.1:9394');
+    server.listen(port, () => {
+        console.log('服务启动(。・∀・)ノ http://127.0.0.1:' + port);
+        child_process.exec(`start chrome http://127.0.0.1:${port}/index.html`);
+    });
 
-    child_process.exec('start chrome http://127.0.0.1:9394/index.html');
-});
+    server.on("error", err => {
+        server.close();
+        if (err.code === "EADDRINUSE") {
+            process.stdout.write(`端口 ${err.port} 已被占用，是否切换端口？(y/n)`);
+            process.stdin.on('data', (input) => {
+                input = input.toString().trim();
+                if (input.startsWith("y")) {
+                    startServer(Math.floor(Math.random()*10000 + 5000));
+                } else {
+                    process.exit(1);
+                }
+            });
+        }
+    });
+}
+
+startServer();
 
 // 版本一： 完成基本请求，不涉及文件（支持https、http）
 // 版本二： 添加网页输入功能，显示功能
 // 版本三： 添加上传文件功能
+
+// process.argv 
 
